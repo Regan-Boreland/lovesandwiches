0e9b1c499d866a3109d9f134ec842476330ffb57
Regan Boreland <reganboreland@hotmail.co.uk>
2024-07-07 20:34:34 +0000

    calculate surplus data

--- run.py
+++ run.py
@@ -1,41 +1,47 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
+
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
     """
-    Gets sales data from user input 
+    Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
-        print("Please enter the sales data from the last market")
-        print("Data should be six numbers, separated by commas")
-        print("Example: 10, 20, 30, 40, 50, 60\n")
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here: ")
-        
-        sales_data = data_str.split(",")
-        validate_data(sales_data)
 
-        if(validate_data(sales_data)):
-            print("Data is valid")
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
             break
-    
+
     return sales_data
+
 
 def validate_data(values):
     """
-    Inside the try, converts all strings into integers.
-    Raises ValueError if strings can't be converted into int,
-    or if there aren't exactly 6 valsues.
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
     """
     try:
         [int(value) for value in values]
@@ -46,7 +52,50 @@ def validate_data(values):
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
-    
+
     return True
 
-data = get_sales_data()
+
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
+
+def calculate_surplus_data(sales_row):
+    """
+    compare sales data with stock to calculate the surplus
+
+    The surplus is defined with the stock data minus the sales data:
+    - positive number indicates waste
+    - negative number indicates extras made after stock was sold
+    """
+    print("calculating surplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+
+    surplus_data = []
+    for stock, sales in zip(sales_row, stock_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+
+    return surplus
+
+def main():
+    """
+    run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
+
+print("welcome to Love Sandwiches Data Automation")
+main()
+
+
